df298d4a4323c83a84b88ec4b3b36720eddb2bc0
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2022-07-21 17:43:07 -0500

    Add POC

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+// @ts-ignore
+import { Reader } from "@transcend-io/conflux";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import Map from "./Map";
+import tiler from "./tiler";
 
 function App() {
+  async function extract() {
+    const zip = await fetch("/downloads/Chicago.zip").then((res) => res.blob());
+
+    console.log(zip);
+
+    let entry = undefined;
+
+    for await (const _entry of Reader(zip)) {
+      if (_entry.name.endsWith(".tif")) {
+        entry = _entry;
+        break;
+      }
+    }
+
+    // I don't understand why logging here makes this work lmao
+    console.log(entry, entry.arrayBuffer());
+
+    const arrayBuffer = await entry.arrayBuffer();
+
+    tiler.postMessage({ file: new Blob([arrayBuffer]) });
+
+    // var img = document.getElementById("image");
+    // img.src = "data:image/png;base64," + encode(await image.readRasters());
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p onClick={extract}>Download and extract chart</p>
+        <Map />
       </header>
     </div>
   );
---
+++ src/Map.tsx
@@ -0,0 +1,115 @@
+import L from "leaflet";
+import { useEffect, useRef } from "react";
+import tiler from "./tiler";
+
+var tileCallbacks = {};
+
+// Calculated min/max/nodata for the file, used for each tile request
+var fileStats;
+
+const WorkerTiles = L.GridLayer.extend({
+  createTile: function (coords, done) {
+    var uLPix = {
+      x: coords.x * 256, // In real life, "this.getTileSize()"
+      y: coords.y * 256,
+    };
+    var lRPix = {
+      x: uLPix.x + 256,
+      y: uLPix.y + 256,
+    }; // Ditto
+
+    var map = this._map; // TODO: Don't rely on Leaflet internals
+    var uLGeo = map.unproject(uLPix, coords.z);
+    var lRGeo = map.unproject(lRPix, coords.z);
+
+    var tile = document.createElement("img");
+    tiler.postMessage({
+      tile: {
+        upperLeft: uLGeo,
+        lowerRight: lRGeo,
+        coords: coords,
+        stats: fileStats,
+      },
+    });
+    var callback = function (bytes) {
+      // This doesn't really seem to make a difference, but it's quicker.
+      // TODO: Make empty tiles not show up as broken images
+      if (bytes.length === 0) {
+        done(null, null);
+      } else {
+        var outputBlob = new Blob([bytes], { type: "image/png" });
+        var imageURL = window.URL.createObjectURL(outputBlob);
+        tile.src = imageURL;
+        done(null, tile); // done(error, tile);
+      }
+    };
+
+    var callbackKey =
+      coords.x.toString() +
+      "," +
+      coords.y.toString() +
+      "," +
+      coords.z.toString();
+    tileCallbacks[callbackKey] = callback;
+    return tile;
+  },
+});
+
+var tiffTiles;
+
+export default function Map() {
+  const mapElRef = useRef<HTMLDivElement>(null);
+  const mapRef = useRef<L.Map>();
+
+  useEffect(() => {
+    if (!mapElRef.current || mapRef.current) return;
+
+    var map = L.map(mapElRef.current).setView([0, 0], 3);
+    mapRef.current = map;
+    map.options.minZoom = 10;
+
+    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      maxZoom: 18,
+    }).addTo(map);
+
+    tiler.onmessage = function (evt) {
+      if (evt.data.tile) {
+        var tileReq = evt.data.tile.request;
+        var callbackKey =
+          tileReq.coords.x.toString() +
+          "," +
+          tileReq.coords.y.toString() +
+          "," +
+          tileReq.coords.z.toString();
+        tileCallbacks[callbackKey](evt.data.tile.bytes);
+        delete tileCallbacks[callbackKey];
+      } else if (evt.data.success) {
+        if (tiffTiles) {
+          tiffTiles.remove();
+        }
+        tiffTiles = new WorkerTiles();
+        var lats = Array.from(evt.data.bounds[1]);
+        var lngs = Array.from(evt.data.bounds[0]);
+
+        // TODO: Remove globals
+        fileStats = evt.data.stats;
+        // Zip
+        var latLngs = lats.map(function (lat, i, arr) {
+          return new Array(lat, lngs[i]);
+        });
+        map.fitBounds(latLngs);
+        tiffTiles.addTo(map);
+      } else {
+        console.log(evt);
+      }
+    };
+  }, [mapElRef]);
+
+  return (
+    <>
+      <div ref={mapElRef} />
+    </>
+  );
+}
