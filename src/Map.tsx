ad4228357bc552673ec581f54118e528122f9c5f
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2022-07-21 17:50:44 -0500

    Fix errors

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,14 +1,14 @@
-import L from "leaflet";
+import L, { Coords, DoneCallback, LatLngBoundsExpression } from "leaflet";
 import { useEffect, useRef } from "react";
 import tiler from "./tiler";
 
-var tileCallbacks = {};
+let tileCallbacks: any = {};
 
 // Calculated min/max/nodata for the file, used for each tile request
-var fileStats;
+let fileStats: any;
 
 const WorkerTiles = L.GridLayer.extend({
-  createTile: function (coords, done) {
+  createTile: function (coords: Coords, done: DoneCallback) {
     var uLPix = {
       x: coords.x * 256, // In real life, "this.getTileSize()"
       y: coords.y * 256,
@@ -31,16 +31,16 @@ const WorkerTiles = L.GridLayer.extend({
         stats: fileStats,
       },
     });
-    var callback = function (bytes) {
+    var callback = function (bytes: any) {
       // This doesn't really seem to make a difference, but it's quicker.
       // TODO: Make empty tiles not show up as broken images
       if (bytes.length === 0) {
-        done(null, null);
+        done(undefined, undefined);
       } else {
         var outputBlob = new Blob([bytes], { type: "image/png" });
         var imageURL = window.URL.createObjectURL(outputBlob);
         tile.src = imageURL;
-        done(null, tile); // done(error, tile);
+        done(undefined, tile); // done(error, tile);
       }
     };
 
@@ -55,7 +55,7 @@ const WorkerTiles = L.GridLayer.extend({
   },
 });
 
-var tiffTiles;
+let tiffTiles: any;
 
 export default function Map() {
   const mapElRef = useRef<HTMLDivElement>(null);
@@ -90,15 +90,15 @@ export default function Map() {
           tiffTiles.remove();
         }
         tiffTiles = new WorkerTiles();
-        var lats = Array.from(evt.data.bounds[1]);
-        var lngs = Array.from(evt.data.bounds[0]);
+        var lats: number[] = Array.from(evt.data.bounds[1]);
+        var lngs: number[] = Array.from(evt.data.bounds[0]);
 
         // TODO: Remove globals
         fileStats = evt.data.stats;
         // Zip
-        var latLngs = lats.map(function (lat, i, arr) {
-          return new Array(lat, lngs[i]);
-        });
+        let latLngs = lats.map(function (lat, i, arr) {
+          return [lat, lngs[i]];
+        }) as LatLngBoundsExpression;
         map.fitBounds(latLngs);
         tiffTiles.addTo(map);
       } else {
